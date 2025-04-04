b0dc32161c9c9812e5c920c3f9e17c15295700f9
PRABHAT1SHUKLA <prabhatshukla0849@gmail.com>
2025-04-04 19:53:26 +0530

    channel

---
+++ channel.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func worker(done chan bool) {
+	fmt.Println("Working...")
+	time.Sleep(2 * time.Second)
+	fmt.Println("Done!")
+	done <- true
+}
+
+func main() {
+	done := make(chan bool)
+	go worker(done)
+	<-done // Wait for goroutine to finish
+}
