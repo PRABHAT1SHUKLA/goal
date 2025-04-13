66236fec2acb8d3c42fed469f95d2db1af097870
PRABHAT1SHUKLA <prabhatshukla0849@gmail.com>
2025-04-13 22:26:49 +0530

    worker

---
+++ worker.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func worker(id int, jobs <-chan int, results chan<- int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for j := range jobs {
+		fmt.Printf("Worker %d started job %d\n", id, j)
+		time.Sleep(time.Second)
+		fmt.Printf("Worker %d finished job %d\n", id, j)
+		results <- j * 2
+	}
+}
+
+func main() {
+	const numJobs = 5
+	const numWorkers = 3
+
+	jobs := make(chan int, numJobs)
+	results := make(chan int, numJobs)
+
+	var wg sync.WaitGroup
+
+	// Start workers
+	for w := 1; w <= numWorkers; w++ {
+		wg.Add(1)
+		go worker(w, jobs, results, &wg)
+	}
+
+	// Send jobs
+	for j := 1; j <= numJobs; j++ {
+		jobs <- j
+	}
+	close(jobs)
+
+	// Wait for all workers
+	wg.Wait()
+	close(results)
+
+	for res := range results {
+		fmt.Println("Result:", res)
+	}
+}
